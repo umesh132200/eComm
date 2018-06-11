b52c8c1e5dda62dcfb68799262572a7b5f52fb09
umesh Kumar <umesh132200@gmail.com>
2018-06-12 01:24:10 +0530

    created logger and handle exceptions

--- app.js
+++ app.js
@@ -7,19 +7,35 @@ const app = express();
 const debug = require('debug');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
-
 const product = require('./api/router/product.router');
 const order = require('./api/router/order.router');
 const customer = require('./api/router/customer.router');
 const cart = require('./api/router/cart.router');
 
-winston.add(winston.transports.File, { filename: 'logger.js'});
-winston.add(winston.transports.MongoDB, {db: 'mongodb://localhost/logger'});
+// process.on('uncaughtException', (ex) => {
+//     console.log('WE GOT AN UNCAUGHT EXCEPTION');
+//     winston.error(ex.message, ex);
+// });
+
+winston.handleExceptions(
+    new winston.transports.Console({ colorize: true, prettyPrint: true }),
+    new winston.transports.File({filename: 'uncaught_unhandled_Exception.log'}));
+
+process.on('unhandledRejection', (ex) => {
+    // console.log('WE GOT AN UNHANDLED REJECTION');
+    // winston.error(ex.message, ex);
+    throw ex;
+});
+
+winston.add(winston.transports.File, { filename: 'logger.log'});
+winston.add(winston.transports.MongoDB, {db: 'mongodb://localhost/logger', level: 'info'});
+
+const p = Promise.reject(new Error('something failed when startup')).then(console.log('done'));
+
 
 //connect to mongodb
 mongoose.connect('mongodb://localhost/ecom')
- .then(() => console.log('connected to mongodb'))
- .catch( err => console.log('Mongodb connection failed'));
+ .then(() => console.log('connected to mongodb'));
 
 
  //middileware
