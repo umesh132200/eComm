b8b8feb4e1c94020f39647fee070065233255a0a
umesh Kumar <umesh132200@gmail.com>
2018-06-16 02:41:13 +0530

    theme placed

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { ContentComponent } from './content/content.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    TopbarComponent,
+    ContentComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
